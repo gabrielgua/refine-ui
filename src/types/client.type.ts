77f3b943d50244f71bf347ad5ecbd1e62003ec4c
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2025-04-24 16:48:00 -0300

    feat: add modal to inform when a wrong type of client is selected on the balanceView

--- src/types/client.type.ts
+++ src/types/client.type.ts
@@ -1,7 +1,10 @@
+import type { CredentialRange } from './credential-range.type'
+
 export type Client = {
   credential: string
   name: string
   role: string
   balance?: number
   freeOfCharge: boolean
+  credentialRange: CredentialRange
 }
